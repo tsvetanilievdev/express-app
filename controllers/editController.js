85d738bf72e00b2c68f8c6b4f54ee53ad45d1aed
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-01-15 22:56:38 +0200

    added update Extra functionallity on Shoes update

--- controllers/editController.js
+++ controllers/editController.js
@@ -1,21 +1,34 @@
+const { getAllExtras } = require('../services/extrasService.js');
 const { getById, editById } = require('../services/shoesService.js');
+const whichBoxIsChecked = require('../utils/whichBoxIsChecked.js');
 
 const router = require('express').Router();
 
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
     const shoes = await getById(id);
+    const allExtras = await getAllExtras();
+    const extras = whichBoxIsChecked(shoes.extras, allExtras);
     res.render('edit', {
-        shoes
+        shoes,
+        extras
     });
 })
 
 router.post('/:id', async (req, res) => {
 
     try {
-        console.log('BEFORE')
-        const result = await editById(req.params.id, req.body);
-        console.log('AFTER')
+        let shoesData = {
+            brand: req.body.brand,
+            model: req.body.model,
+            price: req.body.price,
+            size: req.body.size,
+            image: req.body.image,
+            description: req.body.description,
+        }
+        shoesData.extras = Object.keys(req.body).filter(k => k.startsWith('box')).map(k => k.slice(4))
+
+        const result = await editById(req.params.id, shoesData);
         res.redirect('/catalog/' + result._id);
     } catch (errors) {
         res.locals.errors = errors;
--- utils/whichBoxIsChecked.js
+++ utils/whichBoxIsChecked.js
@@ -1,8 +1,15 @@
 function whichBoxIsChecked(body, data) {
     const extras = data.map(x => Object.assign({ isOn: false }, x))
-    const checkedExtras = body.extras = Object.keys(body).filter(k => k.startsWith('box')).map(k => k.slice(4))
+    console.log(body);
+    let checkedExtras;
+    if (Array.isArray(body)) {
+        checkedExtras = body.map(k => k._id);
+
+    } else {
+        checkedExtras = Object.keys(body).filter(k => k.startsWith('box')).map(k => k.slice(4));
+    }
     extras.forEach(x => {
-        if (checkedExtras.some(c => c == x._id)) {
+        if (checkedExtras.some(c => c.toString() == x._id.toString())) {
             x.isOn = true;
         }
 
