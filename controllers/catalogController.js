3d7ce43b8d3a2fc53af5b8daf86b74fb15709c99
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-01-26 15:32:13 +0200

    finished My Catalog check and validation

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -1,32 +1,6 @@
 const router = require('express').Router();
 const { getAll, getById } = require('../services/shoesService.js');
 
-router.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    try {
-        const shoes = await getById(id);
-        if (shoes != null) {
-            if (req.user && req.user._id == shoes?.ownerId) {
-                shoes.isOwner = true;
-            }
-
-            shoes.isAdmin = res.locals.isAdmin
-            res.render('details', {
-                title: 'Details Page',
-                shoes
-            });
-        } else {
-            throw new Error('Missing record!');
-        }
-    } catch (error) {
-        console.log(error)
-        //casting error ?????? CastError: Cast to ObjectId failed for value
-        res.redirect('/catalog');
-    }
-
-
-});
-
 router.get('/', async (req, res) => {
     const search = req.query.search || '';
     try {
@@ -43,7 +17,30 @@ router.get('/', async (req, res) => {
             search
         });
     }
-
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const shoes = await getById(id);
+        if (shoes != null) {
+            if (req.user && req.user._id == shoes?.ownerId) {
+                shoes.isOwner = true;
+            }
+            shoes.isAdmin = res.locals.isAdmin
+            res.render('details', {
+                title: 'Details Page',
+                shoes
+            });
+        } else {
+            throw new Error('Missing record!');
+        }
+    } catch (error) {
+        console.log(error)
+        //casting error ?????? CastError: Cast to ObjectId failed for value
+        res.redirect('/catalog');
+    }
+});
+
 
 module.exports = router;
--- controllers/myCatalogController.js
+++ controllers/myCatalogController.js
@@ -4,28 +4,44 @@ const { getAll, getById, getMyShoes } = require('../services/shoesService.js');
 
 router.get('/', async (req, res) => {
     const search = req.query.search || '';
-    const shoes = await getMyShoes(req.user._id, search)
-    res.render('catalog', {
-        title: 'Catalog Page',
-        shoes,
-        search,
-        userCatalog: true
-    });
-
+    try {
+        const shoes = await getMyShoes(req.user._id, search)
+        res.render('catalog', {
+            title: 'Catalog Page',
+            shoes,
+            search,
+            userCatalog: true
+        });
+    } catch (error) {
+        res.render('catalog', {
+            title: 'Catalog Page',
+            shoes: [],
+            search,
+            userCatalog: true
+        });
+    }
 });
 
 router.get('/:id', async (req, res) => {
-    let id = req.params.id;
-    const shoes = await getById(id);
-    if (req.user && req.user._id == shoes.ownerId) {
-        shoes.isOwner = true;
+    try {
+        const id = req.params.id;
+        const shoes = await getById(id);
+        if (shoes != null) {
+            if (req.user && req.user._id == shoes?.ownerId) {
+                shoes.isOwner = true;
+            }
+            res.render('details', {
+                title: 'Details Page',
+                shoes
+            });
+        } else {
+            throw new Error('Missing record!');
+        }
+    } catch (error) {
+        console.log(error)
+        //casting error ?????? CastError: Cast to ObjectId failed for value
+        res.redirect('/catalog');
     }
-    shoes.isAdmin = res.locals.isAdmin;
-    res.render('details', {
-        title: 'Details Page',
-        shoes
-    });
-
 });
 
 module.exports = router;
