6bf8655ca8f4189bce22883a07e80134f5014765
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-01-28 20:31:11 +0200

    added AlphaNumeric validation on Create

--- controllers/createController.js
+++ controllers/createController.js
@@ -1,7 +1,9 @@
+const { body, validationResult } = require('express-validator');
 const { getAllExtras } = require('../services/extrasService.js');
 const { create } = require('../services/shoesService.js');
-const { parseErrors } = require('../utils/parseErrors.js');
+const { parseErrors, parseExpressValidatorErrors } = require('../utils/parseErrors.js');
 const whichBoxIsChecked = require('../utils/whichBoxIsChecked.js');
+
 
 const router = require('express').Router();
 
@@ -17,32 +19,49 @@ router.get('/', async (req, res) => {
     }
 })
 
-router.post('/', async (req, res) => {
-    try {
-        let shoesData = {
-            brand: req.body.brand,
-            model: req.body.model,
-            price: req.body.price,
-            size: req.body.size,
-            img: req.body.img,
-            description: req.body.description,
+router.post('/',
+    body('brand', 'Brand must contains only english letters or numbers!')
+        .trim()
+        .isAlphanumeric(),
+    body('model', 'Model must contains only english letters or numbers!')
+        .trim()
+        .isAlphanumeric(),
+    body('price', 'Price must be a number!')
+        .trim()
+        .isNumeric(),
+    body('size', 'Size must be a number!')
+        .trim()
+        .isNumeric(),
+    async (req, res) => {
+        try {
+            const isValid = validationResult(req);
+            if (!isValid.isEmpty()) {
+                throw parseExpressValidatorErrors(isValid);
+            }
+            let shoesData = {
+                brand: req.body.brand,
+                model: req.body.model,
+                price: req.body.price,
+                size: req.body.size,
+                img: req.body.img,
+                description: req.body.description,
+            }
+
+            shoesData.extras = Object.keys(req.body).filter(k => k.startsWith('box')).map(k => k.slice(4))
+            const ownerId = req.user._id;
+            const result = await create(shoesData, ownerId);
+            res.redirect('/catalog/' + result._id);
+        } catch (error) {
+            res.locals.errors = parseErrors(error);
+
+            const dataExtras = await getAllExtras();
+            const extras = whichBoxIsChecked(req.body, dataExtras);
+
+            res.locals.shoes = { ...req.body }
+            res.locals.extras = extras;
+            res.render('create');
         }
-
-        shoesData.extras = Object.keys(req.body).filter(k => k.startsWith('box')).map(k => k.slice(4))
-        const ownerId = req.user._id;
-        const result = await create(shoesData, ownerId);
-        res.redirect('/catalog/' + result._id);
-    } catch (error) {
-        res.locals.errors = parseErrors(error);
-
-        const dataExtras = await getAllExtras();
-        const extras = whichBoxIsChecked(req.body, dataExtras);
-
-        res.locals.shoes = { ...req.body }
-        res.locals.extras = extras
-        res.render('create');
-    }
-})
+    })
 
 
 module.exports = router;
